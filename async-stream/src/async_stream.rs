b31b9176f622628e8e62a2e12426e9f8132805a2
Carl Lerche <me@carllerche.com>
2019-08-10 20:42:47 -0700

    spike implementation

--- async-stream/src/async_stream.rs
+++ async-stream/src/async_stream.rs
@@ -7,12 +7,13 @@ use std::task::{Context, Poll};
 
 pub struct AsyncStream<T, U> {
     rx: Receiver<T>,
+    done: bool,
     generator: U,
 }
 
 impl<T, U> AsyncStream<T, U> {
     pub fn new(rx: Receiver<T>, generator: U) -> AsyncStream<T, U> {
-        AsyncStream { rx, generator }
+        AsyncStream { rx, done: false, generator }
     }
 }
 
@@ -22,6 +23,30 @@ where U: Future<Output = ()>
     type Item = T;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<T>> {
-        unimplemented!();
+        unsafe {
+            let me = Pin::get_unchecked_mut(self);
+
+            if me.done {
+                panic!("poll after done");
+            }
+
+            let mut dst = None;
+            let res = {
+                let _enter = me.rx.enter(&mut dst);
+                Pin::new_unchecked(&mut me.generator).poll(cx)
+            };
+
+            me.done = res.is_ready();
+
+            if dst.is_some() {
+                return Poll::Ready(dst.take());
+            }
+
+            if me.done {
+                Poll::Ready(None)
+            } else {
+                Poll::Pending
+            }
+        }
     }
 }
