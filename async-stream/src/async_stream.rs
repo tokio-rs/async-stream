ab332f0af23115c51a8e43d1024bebbd67894b92
Taiki Endo <te316e89@gmail.com>
2020-10-17 22:01:07 +0900

    Implement Stream::size_hint method on AsyncStream

--- async-stream/src/async_stream.rs
+++ async-stream/src/async_stream.rs
@@ -66,4 +66,12 @@ where
             }
         }
     }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        if self.done {
+            (0, Some(0))
+        } else {
+            (0, None)
+        }
+    }
 }
