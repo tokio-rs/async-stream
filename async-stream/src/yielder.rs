891daed5a150ed7cb161db81b5289375f528cc49
aumetra <aumetra@cryptolab.net>
2024-10-01 20:47:14 +0200

    Use inline const

--- async-stream/src/yielder.rs
+++ async-stream/src/yielder.rs
@@ -34,10 +34,7 @@ pub unsafe fn pair<T>() -> (Sender<T>, Receiver<T>) {
 // Tracks the pointer to `Option<T>`.
 //
 // TODO: Ensure wakers match?
-thread_local! {
-    #[allow(clippy::missing_const_for_thread_local)]
-    static STORE: Cell<*mut ()> = Cell::new(ptr::null_mut())
-}
+thread_local!(static STORE: Cell<*mut ()> = const { Cell::new(ptr::null_mut()) });
 
 // ===== impl Sender =====
 
